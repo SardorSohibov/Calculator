f11af7c0e2a5c7bf6c0ce452a31f34d1a5ec6890
SardorSohibov <soxibovsardor270@gmail.com>
2025-05-07 18:11:42 +0500

    created sqrt() method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,7 @@ public class Calculator {
     public static void add(int a, int b) {
         System.out.println(a + "+" + b + " = " + (a + b));
     }
+    public static void sqrt(int a){
+        System.out.println("sqrt("+a+")"+"="+Math.sqrt(a));
+    }
 }
