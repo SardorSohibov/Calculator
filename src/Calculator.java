23b5301795db89b91adb1bd47e55e9f5144d853e
SardorSohibov <soxibovsardor270@gmail.com>
2025-05-07 18:24:42 +0500

    created abs() method

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,5 +5,8 @@ public class Calculator {
     public static void sqrt(int a){
         System.out.println("sqrt("+a+")"+"="+Math.sqrt(a));
     }
+    public static void abs(int a){
+        System.out.println("abs("+a+")= "+Math.abs(a));
+    }
 
 }
